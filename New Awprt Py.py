7c161cb5d05627307c588a148b5742c60d55e19d
SadEPa <longtaizi628@outlook.com>
2023-02-27 06:16:53 +0800

    初始更改

---
+++ New Awprt Py.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+
+url = "https://www.example.com"  # 替换为你要刷新的网页地址
+
+# 创建Chrome浏览器实例
+driver = webdriver.Chrome()
+
+while True:
+    driver.get(url)  # 打开网页
+    time.sleep(10)   # 等待10秒后刷新
